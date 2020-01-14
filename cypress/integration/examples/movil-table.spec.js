cf009ea8ce55da15a4666baf2d2402494aa6c70d
Jose Juan <pe.dominguezjosejuan@gmail.com>
2020-01-14 10:12:33 -0500

    pruebas de estilos en tabla

--- cypress/integration/examples/movil-table.spec.js
+++ cypress/integration/examples/movil-table.spec.js
@@ -11,22 +11,29 @@ describe('Pruebas a tabla responsiva versión MOVIL', () => {
 	})
 	it('Existe el CAPTION de la tabla', () => {
 		cy.get('[data-cy="table-caption"]')
-			.should('exist');
+			.should('exist')
+			.should('contain', 'Este es el slot del caption');
 	})
 	it('Existe el HEAD de la tabla', () => {
 		cy.get('[data-cy="table-head"]')
 			.should('exist')
 			.find('tr')
-			.should('have.class', 'columns-movil');
+			.should('have.class', 'columns-movil')
+			.children()
+			.should('have.length', 3);
 	})
 	it('Existe el BODY de la tabla', () => {
 		cy.get('[data-cy="table-body"]')
 			.should('exist')
 			.find('tr')
-			.should('have.class', 'table-movil');
+			.should('have.class', 'table-movil')
+			.eq(1)
+			.children()
+			.should('have.length', 5);
 	})
 	it('Existe el FOOTER de la tabla', () => {
 		cy.get('[data-cy="table-footer"]')
-			.should('exist');
+			.should('exist')
+			.should('contain', 'Este es el slot del footer');
 	})
 })
--- cypress/integration/examples/web-table.spec.js
+++ cypress/integration/examples/web-table.spec.js
@@ -10,22 +10,43 @@ describe('Pruebas a tabla responsiva versión WEB', () => {
 	})
 	it('Existe el CAPTION de la tabla', () => {
 		cy.get('[data-cy="table-caption"]')
-			.should('exist');
+			.should('exist')
+			.should('contain', 'Este es el slot del caption');
 	})
 	it('Existe el HEAD de la tabla', () => {
 		cy.get('[data-cy="table-head"]')
 			.should('exist')
 			.find('tr')
-			.should('not.have.class', 'columns-movil');
+			.should('not.have.class', 'columns-movil')
+			.children()
+			.should('have.length', 5);
 	})
 	it('Existe el BODY de la tabla', () => {
 		cy.get('[data-cy="table-body"]')
 			.should('exist')
 			.find('tr')
-			.should('not.have.class', 'table-movil');
+			.should('not.have.class', 'table-movil')
+			.eq(0)
+			.invoke('css', 'background-color')
+			.should('equal', 'rgb(240, 248, 255)');
+		cy.get('[data-cy="table-body"]')
+			.should('exist')
+			.find('tr')
+			.should('not.have.class', 'table-movil')
+			.eq(1)
+			.invoke('css', 'background-color')
+			.should('equal', 'rgba(0, 0, 0, 0)');
+		cy.get('[data-cy="table-body"]')
+			.should('exist')
+			.find('tr')
+			.should('not.have.class', 'table-movil')
+			.eq(0)
+			.children()
+			.should('have.length', 5);
 	})
 	it('Existe el FOOTER de la tabla', () => {
 		cy.get('[data-cy="table-footer"]')
-			.should('exist');
+			.should('exist')
+			.should('contain', 'Este es el slot del footer');
 	})
 })
